9303baa359de3c082f7f62d722da744bb50d8071
Avaxar <44055981+avaxar@users.noreply.github.com>
2025-01-26 14:15:29 +0700

    press space to continue

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -6,8 +6,8 @@ func _process(delta: float) -> void:
 	time += delta
 	
 	# I HAVE NO IDEA HOW TO USE ANIMATION PLAYERS
-	for i in range(1, 8):
-		var frame: Sprite2D = get_node(str(i))
+	for i in range(1, 9):
+		var frame := get_node(str(i))
 		var x := clampf(time / 2.5 - i + 1.0, 0.0, 1.0)
 		if frame.modulate.a == 0.0 and x > 0.0:
 			match i:
